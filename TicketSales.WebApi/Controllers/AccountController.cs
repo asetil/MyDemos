be60d77a1e711e2bc812f0fc00d9424399a52263
Osman Sokuoğlu <osman.sokuoglu@gmail.com>
2024-12-14 14:37:31 +0300

    [feature] seed data & search params improvements

--- TicketSales.WebApi/Controllers/AccountController.cs
+++ TicketSales.WebApi/Controllers/AccountController.cs
@@ -1,8 +1,8 @@
 ﻿using Aware.Auth;
-using Aware.BL.Model;
 using Aware.Model;
 using Aware.Search;
 using Aware.Util;
+using Aware.Util.Cache;
 using Aware.Util.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,18 +10,21 @@ using TicketSales.WebApi.Model.Dto;
 
 namespace TicketSales.WebApi.Controllers;
 
-public class AccountController(IUserManager userManager) : AwareEmptyController
+public class AccountController(IUserManager userManager, IAwareCacher cacher) : AwareEmptyController
 {
     [HttpPost("login")]
     [AllowAnonymous]
     public OperationResult<SessionItemDto> Login([FromBody] LoginItemDto model)
     {
+        if (CurrentUserId > 0)
+            return Failed<SessionItemDto>(ResultCodes.Error.Login.AlreadyLoggedIn);
+
         var loginResult = userManager.Login(model.Username, model.Password);
 
         return loginResult;
     }
 
-    [HttpPost]
+    [HttpPost("logout")]
     [Authorize]
     public OperationResult<bool> Logout()
     {
@@ -37,5 +40,25 @@ public class AccountController(IUserManager userManager) : AwareEmptyController
     {
         var result = userManager.Search(searchParams);
         return result;
+    }
+
+    /// <summary>
+    /// FOR DEVELOPMENT PURPOSES ONLY!!!
+    /// Replace user name and password with your own
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("login-demo")]
+    public OperationResult<SessionItemDto> LoginDemo()
+    {
+        if (CurrentUserId > 0)
+            return Failed<SessionItemDto>(ResultCodes.Error.Login.AlreadyLoggedIn);
+
+        var loginResult = userManager.Login("admin@ticketsales.com", "admin");
+        if (loginResult.Ok)
+        {
+            cacher.Add("DEMO_SESSION_TOKEN", loginResult.Value.SessionToken, CommonConstants.HourlyCacheTime);
+        }
+
+        return loginResult;
     }
 }
